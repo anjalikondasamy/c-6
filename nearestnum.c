dd57b6274800a99b89c50e1bd20f60ba6a41bd13
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-03-07 16:48:09 +0530

    nearestnum.c

---
+++ nearestnum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+
+printf("enter the number");
+scanf("%d",&n);
+
+if((n%2)==0)
+{
+printf("nearest even is  %d",n);
+}
+else
+{
+printf("nearest even is %d",n);
+}
+} 
